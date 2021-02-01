0875724fe2c0dd1a850cf333f846c5c4181eaadf
Novikoviliya <juniorwebdevel@gmail.com>
2021-02-01 18:11:58 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const  getRandomValue = ( (min, max) => {
+const  getRandomValue =  (min, max) => {
   const minValue = Math.ceil(min);
   const maxValue = Math.floor(max);
   return Math.random() * (maxValue - minValue + 1) + minValue;
-})
-getRandomValue();
+}
+getRandomValue(1,5);
 
-const  getRandom = ((min, max) => Math.random() * (max - min) + min)
-getRandom();
+const  getRandom = (min, max) => Math.random() * (max - min) + min;
+getRandom(1,5);
