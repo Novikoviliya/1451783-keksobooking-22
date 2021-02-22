6be8478d56f02dabfc416214a3776e9c2d7ce706
Novikoviliya <juniorwebdevel@gmail.com>
2021-02-22 05:06:35 +0300

    ...

--- js/map.js
+++ js/map.js
@@ -1,46 +1,39 @@
 'use strict';
 /* global L:readonly */
-import { deactivateMapForm, activateMapForm, fillAddress } from './form.js';
+import { fillAddress } from './form.js';
 import { createGame } from './data.js';
 import { renderCard } from './card.js';
-deactivateMapForm();
-const map = L.map('map-canvas')
-  .on('load', () => { activateMapForm(); })
-  .setView({
+
+const fillMap = (map) => {
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  const mainIcon = L.icon({
+    iconUrl: 'img/main-pin.svg',
+    iconSize: [45, 45],
+    iconAnchor: [45 / 2, 45],
+  });
+
+  const mainMarker = L.marker({
     lat: 35.6895,
     lng: 139.692,
-  }, 13);
+  }, {
+    draggable: true,
+    icon: mainIcon,
+  }).addTo(map);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-
-const mainIcon = L.icon({
-  iconUrl: 'img/main-pin.svg',
-  iconSize: [45, 45],
-  iconAnchor: [45 / 2, 45],
-});
-
-const mainMarker = L.marker({
-  lat: 35.6895,
-  lng: 139.692,
-}, {
-  draggable: true,
-  icon: mainIcon,
-}).addTo(map);
-
-const onMove = (evt) => {
-  const addressLoc = {
-    lat: evt.target.getLatLng().lat,
-    long: evt.target.getLatLng().lng,
+  const onMove = (evt) => {
+    const addressLoc = {
+      lat: evt.target.getLatLng().lat,
+      long: evt.target.getLatLng().lng,
+    }
+    fillAddress(addressLoc);
   }
-  fillAddress(addressLoc);
-}
-mainMarker.on('move', onMove);
-//Всплывашка
-const adCards = createGame();
 
-const createMap = () => {
+  mainMarker.on('move', onMove);
+  //Всплывашка
+  const adCards = createGame();
   adCards.forEach(({ author, offer, location }) => {
     const icon = L.icon({
       iconUrl: 'img/pin.svg',
@@ -64,6 +57,6 @@ const createMap = () => {
         },
       );
   });
+  return map;
 }
-createMap();
-export { createMap };
+export { fillMap };
